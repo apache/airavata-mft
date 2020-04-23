c22fca53217cf94208cbd7dd966ea3c55ff67ff8
pokearu <aramali@iu.edu>
2020-04-23 11:35:08 -0400

    Adding Dropbox transport files and pom file changes

---
+++ transport/dropbox-transport/src/main/java/org/apache/airavata/mft.dropbox.transport/DropboxReceiver.java
@@ -0,0 +1,21 @@
+package org.apache.airavata.mft.dropbox.transport;
+
+import org.apache.airavata.mft.core.ConnectorContext;
+import org.apache.airavata.mft.core.api.Connector;
+
+public class DropboxReceiver implements Connector{
+    @Override
+    public void init(String resourceId, String credentialToken, String resourceServiceHost, int resourceServicePort, String secretServiceHost, int secretServicePort) throws Exception {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+    @Override
+    public void startStream(ConnectorContext context) throws Exception {
+
+    }
+}
