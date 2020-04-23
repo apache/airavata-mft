70ebcbb8ceddee22a283a7c5e82586cdf1cd6512
pokearu <aramali@iu.edu>
2020-04-23 11:35:08 -0400

    Adding Dropbox transport files and pom file changes

---
+++ transport/dropbox-transport/src/main/java/org/apache/airavata/mft.dropbox.transport/DropboxMetadataCollector.java
@@ -0,0 +1,21 @@
+package org.apache.airavata.mft.dropbox.transport;
+
+import org.apache.airavata.mft.core.ResourceMetadata;
+import org.apache.airavata.mft.core.api.MetadataCollector;
+
+public class DropboxMetadataCollector implements MetadataCollector{
+    @Override
+    public void init(String resourceServiceHost, int resourceServicePort, String secretServiceHost, int secretServicePort) {
+
+    }
+
+    @Override
+    public ResourceMetadata getGetResourceMetadata(String resourceId, String credentialToken) throws Exception {
+        return null;
+    }
+
+    @Override
+    public Boolean isAvailable(String resourceId, String credentialToken) throws Exception {
+        return null;
+    }
+}
