1608b15186472e7e82d3864b9fb132210efdd072
Dimuthu Wannipurage <dimuthu.upeksha2@gmail.com>
2022-03-03 00:52:46 -0500

    Migrating to log4j 2.16

--- agent/src/main/resources/distribution/bin/agent.sh
+++ agent/src/main/resources/distribution/bin/agent.sh
@@ -23,7 +23,7 @@ CWD="$PWD"
 cd ${AIRAVATA_HOME}/bin
 LOGO_FILE="logo.txt"
 
-JAVA_OPTS="-Dspring.config.location=${AIRAVATA_HOME}/conf/ -Dairavata.home=${AIRAVATA_HOME} -Dlogback.configurationFile=file:${AIRAVATA_HOME}/conf/logback.xml"
+JAVA_OPTS="-Dspring.config.location=${AIRAVATA_HOME}/conf/ -Dairavata.home=${AIRAVATA_HOME} -Dlog4j.configurationFile=file:${AIRAVATA_HOME}/conf/log4j2.xml"
 AIRAVATA_COMMAND=""
 EXTRA_ARGS=""
 SERVERS=""
