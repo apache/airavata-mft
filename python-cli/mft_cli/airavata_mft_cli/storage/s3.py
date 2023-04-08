98d2a2b155028e5e5f2eedcd91915ce1a4c163a0
impiyush83 <nalawadepiyush@gmail.com>
2023-04-08 05:09:54 -0400

    #94 Remove unwanted changes

--- python-cli/mft_cli/airavata_mft_cli/storage/s3.py
+++ python-cli/mft_cli/airavata_mft_cli/storage/s3.py
@@ -34,10 +34,10 @@ from airavata_mft_cli import config as configcli
 def handle_add_storage():
     session_token = ""
     aws_regions = ["us-east-2", "us-east-1", "us-west-1", "us-west-2", "af-south-1", "ap-east-1", "ap-east-1",
-                "ap-southeast-3", "ap-south-1", "ap-northeast-3", "ap-northeast-2", "ap-southeast-1",
-                "ap-southeast-2", "ap-northeast-1", "ca-central-1", "cn-north-1", "cn-northwest-1", "eu-central-1",
-                "eu-west-1", "eu-west-2", "eu-south-1", "eu-west-3", "eu-north-1", "eu-south-2", "eu-central-2",
-                "sa-east-1", "me-south-1", "me-central-1", "us-gov-east-1", "us-gov-west-1"]
+                    "ap-southeast-3", "ap-south-1", "ap-northeast-3", "ap-northeast-2", "ap-southeast-1",
+                    "ap-southeast-2", "ap-northeast-1", "ca-central-1", "cn-north-1", "cn-northwest-1", "eu-central-1",
+                    "eu-west-1", "eu-west-2", "eu-south-1", "eu-west-3", "eu-north-1", "eu-south-2", "eu-central-2",
+                    "sa-east-1", "me-south-1", "me-central-1", "us-gov-east-1", "us-gov-west-1"]
 
     options = ["Through AWS Cli config file", "Enter manually" ]
     option, index = pick(options, "How do you want to load credentials", indicator="=>")
